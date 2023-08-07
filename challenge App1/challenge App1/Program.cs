dd066a08e802366c4b5322712415ec94d34ea0d1
sebastian mandela <s.mandela@tlen.pl>
2023-08-07 19:38:26 +0200

    zadanie 4

--- challenge App1/challenge App1/Program.cs
+++ challenge App1/challenge App1/Program.cs
@@ -1,2 +1,16 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿string name = "Ewa";
+var sex = "woman";
+int age = 17;
+
+if (sex == "woman" && age < 30)
+{
+    Console.WriteLine("Kobieta poniżej 30 lat");
+}
+else if (name == "Ewa" && age == 33)
+{
+    Console.WriteLine("Ewa, lat 33");
+}
+else if (sex != "Woman" && age<18)
+{
+    Console.WriteLine("Niepełnoletni  Mężczyzna");
+}
